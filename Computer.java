911ede275a6575cb9b1c8990b9d36fa670932e26
Kgntrkmn <justxmt0@gmail.com>
2025-05-12 10:19:52 +0200

    computerplaymove

--- Computer.java
+++ Computer.java
@@ -5,6 +5,8 @@ public class Computer {
 		current = start;
 	}
 	Coordinate current;
+	Stack CurrentPath = new Stack(1000);
+	Stack memoryStack = new Stack(1000);
 	public Stack findPath(Coordinate targetCoordinate) {
 		Stack path = new Stack(1000);
 		Coordinate currentCoordinate = new Coordinate(current.x, current.y);
@@ -35,30 +37,24 @@ public class Computer {
 			else {
 				path.Pop();
 				currentCoordinate = (Coordinate)path.Peek();
-			}
-			//System.out.println(currentCoordinate.x + " , " + currentCoordinate.y);
-			/*else {
-				if(Player.mapData[currentCoordinate.x + 1][currentCoordinate.y] != '#') {
-					currentCoordinate.x++;
-				}
-				else if(Player.mapData[currentCoordinate.x][currentCoordinate.y - 1] != '#') {
-					currentCoordinate.y--;
-				}
-				else if(Player.mapData[currentCoordinate.x - 1][currentCoordinate.y] != '#') {
-					currentCoordinate.x--;
-				}
-				else {
-					currentCoordinate.y++;
-				}
-			}*/
-			
-			//Daha önce ordan geçmiş mi diye kontrol etmek için	
 			Player.mapData[currentCoordinate.x][currentCoordinate.y] = '^'; 
-			
 		}
-		
+		}
 		return path;
-		
-		
+	}
+	public void PlayMove() {
+		if (CurrentPath.isEmpty()) // buraya treasure yenmiş mi diye kontrol edilmeli
+			CurrentPath = findPath(new Coordinate(9,9)); // buraya random kordinat gelmeli
+		Coordinate last = new Coordinate(0,0);
+		while (!CurrentPath.isEmpty()) {
+			last = (Coordinate)CurrentPath.Pop();
+			memoryStack.Push(last);
+		}
+		memoryStack.Pop();
+		current = last; // bu koda orası engel mi diye bakmalı
+		while (!memoryStack.isEmpty()) {
+			last = (Coordinate)memoryStack.Pop();
+			CurrentPath.Push(last);
+		}
 	}
 }
