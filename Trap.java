e3a671e70a72c39f37a4e8dae5030bf3f82ab1f6
ece-zsrc <ozsaracece07@gmail.com>
2025-05-15 14:20:05 +0300

    Update Trap.java

--- Trap.java
+++ Trap.java
@@ -1,5 +1,45 @@
 package pbb_project2;
 
-public class Trap {
-
+public class Trap 
+{
+	private int x,y;
+	private long startTime;
+	private boolean active;
+	
+	public Trap()
+	{
+		active = false;
+	}
+	
+	public boolean isActive()
+	{
+		return active;
+	}
+	
+	public void Place(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+		this.startTime = NumberSnake.timer;
+		active = true;
+  }
+	public boolean isTheTimeUp()
+	{
+		boolean timeisup = false;
+		if (active == true && NumberSnake.timer - startTime >= 10)
+		{
+			timeisup = true;
+		}
+		
+		return timeisup;
+	}
+	
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
 }
